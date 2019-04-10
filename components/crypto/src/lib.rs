9b3e291eac3f6cdbfa248fef452b70ef67cb861f
Paweł Zmarzły <pawo2500@gmail.com>
2019-04-10 23:14:43 +0200

    working

--- components/crypto/src/lib.rs
+++ components/crypto/src/lib.rs
@@ -22,30 +22,15 @@ pub mod sym_encrypt;
 pub mod test_utils;
 pub mod uid;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+use derive_more::*;
+
+#[derive(Clone, Copy, Debug, PartialEq, Display)]
+#[display(fmt = "crypto error")]
 pub struct CryptoError;
 
 impl From<::ring::error::Unspecified> for CryptoError {
     fn from(_: ::ring::error::Unspecified) -> CryptoError {
         CryptoError
-    }
-}
-
-impl ::std::fmt::Display for CryptoError {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-        f.write_str("crypto error")
-    }
-}
-
-impl ::std::error::Error for CryptoError {
-    #[inline]
-    fn description(&self) -> &str {
-        "crypto error"
-    }
-
-    #[inline]
-    fn cause(&self) -> Option<&::std::error::Error> {
-        None
     }
 }
 
