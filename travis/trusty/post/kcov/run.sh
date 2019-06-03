fe046da3dd9e57e8fedb71e9a760aaeb4d2d5f9a
Paweł Zmarzły <pawo2500@gmail.com>
2019-06-03 23:39:25 +0200

    Cleaner kcov debugging

--- travis/trusty/post/kcov/run.sh
+++ travis/trusty/post/kcov/run.sh
@@ -2,17 +2,9 @@
 
 # CODECOV_TOKEN Must be set at this point.
 
-function_pstree(){
-    while :
-    do
-        pstree
-        sleep 10
-    done
-}
-function_pstree &
-
 exes=$(find target/${TARGET}/debug -maxdepth 1 -executable -type f)
 for exe in ${exes}; do
+    echo ">>> kcov: " ${exe}
     ${HOME}/install/kcov-${TARGET}/bin/kcov \
         --verify \
         --exclude-path=/usr/include \
