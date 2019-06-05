f5ed19d273b945cdb64f88ba4f2daa05134cfa7f
Paweł Zmarzły <pawo2500@gmail.com>
2019-06-05 18:43:54 +0200

    cache clear, more timestamps

--- travis/trusty/post/kcov/run.sh
+++ travis/trusty/post/kcov/run.sh
@@ -4,7 +4,7 @@
 
 exes=$(find target/${TARGET}/debug -maxdepth 1 -executable -type f)
 for exe in ${exes}; do
-    echo ">>> kcov: " ${exe}
+    echo ">>> kcov: " ${exe} | ts '[%M:%.S]'
     ${HOME}/install/kcov-${TARGET}/bin/kcov \
         --verify \
         --exclude-pattern=/.cargo,/usr/lib \
